e66e62bad229bd32cb492b40e3fd38a7acc128a6
yeasin <mdkawsarislam2002@gmail.com>
2025-02-10 00:17:46 +0600

    chore: updated eslint plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import { FlatCompat } from '@eslint/eslintrc';
+import unicorn from 'eslint-plugin-unicorn';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -10,7 +11,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript')
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      unicorn
+    },
+    rules: {
+      'unicorn/prefer-module': 'error',
+      'unicorn/filename-case': [
+        'error',
+        { case: 'kebabCase', ignore: ['^README\\.md$', '.*\\.config\\.js$'] }
+      ],
+      'unicorn/no-useless-spread': 'error'
+    }
+  }
 ];
 
 export default eslintConfig;
