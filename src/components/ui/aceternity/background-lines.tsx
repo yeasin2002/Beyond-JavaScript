6c37aa4894a063483c68de700da6e407b88fdd1b
al-imam <alimam01828@gmail.com>
2025-02-13 23:51:12 +0600

    chore: migrate dependency

--- src/components/ui/aceternity/background-lines.tsx
+++ src/components/ui/aceternity/background-lines.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { cn } from '@/utils';
-import { motion } from 'framer-motion';
+import { motion } from 'motion/react';
 import React from 'react';
 
 export const BackgroundLines = ({
